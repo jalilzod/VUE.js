57fe7f299bcbe137bc13efa5af2d5a43ce944009
Ming <mingyuan_12@qq.com>
2024-11-03 20:19:55 +0800

    model-v

--- 00VueBasic/myVuePack/src/App.vue
+++ 00VueBasic/myVuePack/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import LoopVue from './components/05Loops/LoopVue.vue'
+import ModelVue from './components/06Models/ModelVue.vue'
+import o2UserForm from './components/01Events/o2UserForm.vue'
 import o1Event from './components/01Events/o1Event.vue'
 </script>
 
 <template>
-  <LoopVue />
+  <ModelVue />
 </template>
 
 <style scoped></style>
---
+++ 00VueBasic/myVuePack/src/components/06Models/ModelVue.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {ref} from 'vue'
+
+const formatData = ref({
+  userName:'',
+  email:'',
+  password:''
+});
+</script>
+
+
+<template>
+  <form class="form">
+    <input class="input" type="text" placeholder="Enter your name" v-model="formatData.userName">
+    <input class="input" type="text" placeholder="Enter your email" v-model="formatData.email">
+    <input class="input" type="password" placeholder="Enter your password" v-model="formatData.password">
+
+  </form>
+    <h1>{{ formatData.userName }}</h1>
+    <h1>{{ formatData.email }}</h1>
+    <h1>{{ formatData.password }}</h1>
+</template>
+
+
+<style>
+h1{
+  margin: 0;
+  text-align: center;
+  font-size: 24px;
+  color: rgb(3, 3, 86);
+}
+</style>
