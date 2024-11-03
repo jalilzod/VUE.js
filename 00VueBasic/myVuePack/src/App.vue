aeab86047513a519194c1d4dd7019b2a07f5a199
Ming <mingyuan.cs98@gmail.com>
2024-11-03 15:26:58 +0800

    completed user form

--- 00VueBasic/myVuePack/src/App.vue
+++ 00VueBasic/myVuePack/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
+import o2UserForm from './components/01Events/o2UserForm.vue'
 import o1Event from './components/01Events/o1Event.vue'
 </script>
 
 <template>
-  <o1Event />
+  <o2UserForm />
 </template>
 
 <style scoped></style>
---
+++ 00VueBasic/myVuePack/src/components/01Events/o2UserForm.vue
@@ -0,0 +1,33 @@
+<script setup>
+const submitHandler = e => {
+  e.preventDefault()
+
+  alert('Valid data')
+}
+</script>
+
+<template>
+  <form class="form" @:submit="submitHandler">
+    <input class="input" type="text" placeholder="Enter your first name" />
+    <input class="input" type="text" placeholder="Enter your last name" />
+    <input class="input" type="password" placeholder="Enter password" />
+    <button class="btn incrBtn" type="submit">Log In</button>
+  </form>
+</template>
+
+<style>
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 1rem;
+}
+
+.input {
+  border-radius: 4px;
+  border: 1.5px solid gray;
+  outline: none;
+  padding: 4px 4px;
+}
+</style>
