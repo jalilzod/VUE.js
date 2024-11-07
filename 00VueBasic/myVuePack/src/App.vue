5e3cc54e59b7ef2c797599813bf686b9ab21c37e
Ming <mingyuan_12@qq.com>
2024-11-07 15:38:13 +0800

    Props initial

--- 00VueBasic/myVuePack/src/App.vue
+++ 00VueBasic/myVuePack/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import ModelVue from './components/06Models/ModelVue.vue'
+import PropsVue from './components/07Props/PropsVue.vue'
 import o2UserForm from './components/01Events/o2UserForm.vue'
 import o1Event from './components/01Events/o1Event.vue'
+
+
 </script>
 
 <template>
-  <ModelVue />
+  <PropsVue />
 </template>
 
 <style scoped></style>
---
+++ 00VueBasic/myVuePack/src/components/07Props/PropsVue.vue
@@ -0,0 +1,15 @@
+
+
+<script setup>
+
+
+</script>
+
+
+<template>
+  <h1>Hello</h1>
+</template>
+
+
+<style>
+</style>
