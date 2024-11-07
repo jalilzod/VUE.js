65ccaead8733a039ddd72128678eece12d8fa412
Ming <mingyuan_12@qq.com>
2024-11-07 18:19:49 +0800

    provide inject initial

--- 00VueBasic/myVuePack/src/App.vue
+++ 00VueBasic/myVuePack/src/App.vue
@@ -1,26 +1,17 @@
 <script setup>
-import ChildToParent from './components/08ComponentEvent/ChildToParent.vue'
+import ProvInject from './components/11ProvideInject/ProvInject.vue'
 import o2UserForm from './components/01Events/o2UserForm.vue'
 import o1Event from './components/01Events/o1Event.vue'
 import{ref} from 'vue'
 // const firstName = ref('Ming');
 // const lastName = ref('Yuan');
 
-const count = ref(0);
 
-const handleUserForm = (userName,userEmail,userPassword)=>{
-
-  console.log("User: ",userName);
-  console.log("Email: ",userEmail);
-  console.log("Password: ",userPassword);
-
-}
 
 </script>
 
 <template>
-  <h1>Count: {{ count }}</h1>
-   <ChildToParent @userForm="handleUserForm"/>
+   <ProvInject />
 </template>
 
 <style scoped></style>
---
+++ 00VueBasic/myVuePack/src/components/10Slot/SlotVue.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+import {ref} from 'vue'
+
+let txt = ref('Sign In');
+
+let Ru = ()=>{
+  txt.value = 'Войти'
+}
+
+let En = ()=>{
+  txt.value = 'Log In'
+}
+
+</script>
+
+
+
+<template>
+  <h1>Hello</h1>
+  <button class="btn hl">{{ txt }}</button>
+  <button v-on:click="Ru" class="btn incrBtn">Ru</button>
+  <button v-on:click="En" class="btn incrBtn">En</button>
+</template>
+
+<style>
+
+.hl{
+  background-color: darkred;
+}
+
+</style>
