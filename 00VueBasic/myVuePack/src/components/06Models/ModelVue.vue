3988029bd9643d215f4a4d22ff373cf07049b9c9
Ming <mingyuan_12@qq.com>
2024-11-07 15:35:33 +0800

    complete register and models

--- 00VueBasic/myVuePack/src/components/06Models/ModelVue.vue
+++ 00VueBasic/myVuePack/src/components/06Models/ModelVue.vue
@@ -6,19 +6,25 @@ const formatData = ref({
   email:'',
   password:''
 });
+
+function handleRegister(){
+  alert('Successfull...');
+}
+
 </script>
 
 
 <template>
-  <form class="form">
+  <form class="form" @submit.prevent="handleRegister">
     <input class="input" type="text" placeholder="Enter your name" v-model="formatData.userName">
-    <input class="input" type="text" placeholder="Enter your email" v-model="formatData.email">
+    <input class="input" type="email" placeholder="Enter your email" v-model="formatData.email">
     <input class="input" type="password" placeholder="Enter your password" v-model="formatData.password">
-
+    <button class="btn incrBtn" type="submit">Submit</button>
   </form>
     <h1>{{ formatData.userName }}</h1>
     <h1>{{ formatData.email }}</h1>
     <h1>{{ formatData.password }}</h1>
+
 </template>
 
 
