ec06f52678d0db4dfce5d668361aae30cb362f42
Ming <mingyuan_12@qq.com>
2024-11-07 16:11:28 +0800

    props finished

--- 00VueBasic/myVuePack/src/App.vue
+++ 00VueBasic/myVuePack/src/App.vue
@@ -2,12 +2,15 @@
 import PropsVue from './components/07Props/PropsVue.vue'
 import o2UserForm from './components/01Events/o2UserForm.vue'
 import o1Event from './components/01Events/o1Event.vue'
+import{ref} from 'vue'
+const firstName = ref('Ming');
+const lastName = ref('Yuan');
 
 
 </script>
 
 <template>
-  <PropsVue />
+  <PropsVue :name="firstName" :last="lastName"/>
 </template>
 
 <style scoped></style>
--- 00VueBasic/myVuePack/src/components/07Props/PropsVue.vue
+++ 00VueBasic/myVuePack/src/components/07Props/PropsVue.vue
@@ -1,13 +1,16 @@
 
 
 <script setup>
-
-
+  const props = defineProps(['name','last']);
+ //we cannot change the value of propss
+  console.log(props);
 </script>
 
 
 <template>
-  <h1>Hello</h1>
+  <h1>{{ props.name  }}</h1>
+  <h1>{{ props.last  }}</h1>
+
 </template>
 
 
