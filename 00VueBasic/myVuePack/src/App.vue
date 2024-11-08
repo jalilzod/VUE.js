d52db140e129f055d7101999805838417c3c448d
Ming <mingyuan_12@qq.com>
2024-11-08 19:44:18 +0800

    Mount Unmount initial

--- 00VueBasic/myVuePack/src/App.vue
+++ 00VueBasic/myVuePack/src/App.vue
@@ -1,33 +1,12 @@
 <script setup>
-import PracticeComponent from './components/11ProvideInject/PracticeComponent.vue'
 import o2UserForm from './components/01Events/o2UserForm.vue'
 import o1Event from './components/01Events/o1Event.vue'
-import {provide} from 'vue'
 
-provide(
-  'admin',
-  ['ming','yuan','yang'],
-)
-
-provide(
-  'games',{
-    id:1,
-    title:'Mafia',
-    genre: ['action','advanture','rpg'],
-    platform: ['pc','xBox','PlayStation'],
-    developer: {
-      name: 'mingyuan',
-      location: 'Shanghai',
-      teamSize: 50
-    }
-  })
-
-
+import MountUn from './components/12LifeCycle/MountUn.vue'
 
 </script>
 
-<template>
-   <PracticeComponent />
-</template>
 
-<style scoped></style>
+<template>
+  <MountUn />
+</template>
