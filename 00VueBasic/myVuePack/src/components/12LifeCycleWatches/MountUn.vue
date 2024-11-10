b9ac886111bdddd6aed839363d77977ac6f06d22
Ming <mingyuan_12@qq.com>
2024-11-10 22:07:03 +0800

    update

---
+++ 00VueBasic/myVuePack/src/components/12LifeCycleWatches/MountUn.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {ref} from 'vue'
+
+
+let message   =  ref('You should learn  js and vue');
+
+
+
+</script>
+
+
+<template>
+  <h1>{{ message }}</h1>
+  <button>Change text</button>
+</template>
