01466c24a1aff2638f6ebd1a734693d4a7629a12
jalilzod_art <mingyuan_12@qq.com>
2025-05-23 23:24:26 +0800

    props in vue

--- vue-basic-2025/src/App.vue
+++ vue-basic-2025/src/App.vue
@@ -1,9 +1,27 @@
 <script setup>
-import ComputedProp from './components/ComputedProp.vue';
+import StaticProps from './components/StaticProps.vue';
+import {ref} from 'vue';
+
+const firstN = ref('Mingyuan');
+const lastN = ref('Yuan');
+let tmpN = ref('');
+let tmpF = ref('');
+
+
+const changeName = ()=>{
+  firstN.value = tmpN.value; 
+  lastN.value = tmpF.value; 
+
+  console.log(tmpN.value+tmpF.value);
+}
+
 </script>
 
 <template>
   <div>
-   <ComputedProp />
+    <input type="text" placeholder="Enter f name" v-model="tmpN">
+    <input type="text" placeholder="Enter l name" v-model="tmpF">
+    <button @click="changeName">Change name</button>
+   <StaticProps :firstName="firstN" :lastName="lastN"/>
   </div>
 </template>
---
+++ vue-basic-2025/src/components/StaticProps.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+
+const dataFromParent = defineProps(
+    ['firstName',
+    'lastName'
+    ]
+);
+
+
+
+</script>
+
+
+<template>
+    
+    <div>
+        <h1>{{ dataFromParent.firstName }}</h1>
+        <h1>{{ dataFromParent.lastName }}</h1>
+    </div>
+
+</template>
